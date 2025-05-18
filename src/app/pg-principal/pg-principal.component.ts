6cf27046a147df530c7eb3f3d0d226fe8d16b601
luiz <luiz.c.lacerda.Psicanalise@gmail.com>
2025-05-18 18:13:54 -0300

    nova div com texto

--- src/app/pg-principal/pg-principal.component.ts
+++ src/app/pg-principal/pg-principal.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-pg-principal',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './pg-principal.component.html',
   styleUrl: './pg-principal.component.scss'
 })
 export class PgPrincipalComponent {
 
+
+  mostrarOutraDiv = false;
+
+@HostListener('window:wheel', ['$event'])
+onWheel(event: WheelEvent) {
+  if (event.deltaY > 0) {
+    this.mostrarOutraDiv = true;
+  } else {
+    this.mostrarOutraDiv = false;
+  }
+}
 }
