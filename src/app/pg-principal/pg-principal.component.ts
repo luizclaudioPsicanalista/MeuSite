28a01f2aed3ee866166218fcd7fb9f8c23f8ce58
luiz lacerda <luizfelipe_lacerda@ymail.com>
2025-05-18 15:32:30 -0300

    first commit

---
+++ src/app/pg-principal/pg-principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pg-principal',
+  standalone: true,
+  imports: [],
+  templateUrl: './pg-principal.component.html',
+  styleUrl: './pg-principal.component.scss'
+})
+export class PgPrincipalComponent {
+
+}
