e5531ad200532bf6b60c0cfb4bd73b5f12eec281
luiz <luiz.c.lacerda.Psicanalise@gmail.com>
2025-05-19 09:17:56 -0300

    ajuste  divs com texto

--- src/app/pg-principal/pg-principal.component.ts
+++ src/app/pg-principal/pg-principal.component.ts
@@ -13,20 +13,5 @@ export class PgPrincipalComponent {
 
   mostrarDiv1 = true;
 
-  private lastScrollTop = 0;
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollTop > this.lastScrollTop) {
-      // scroll pra baixo
-      this.mostrarDiv1 = false;
-    } else if (scrollTop < this.lastScrollTop) {
-      // scroll pra cima
-      this.mostrarDiv1 = true;
-    }
-
-    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-  }
 }
